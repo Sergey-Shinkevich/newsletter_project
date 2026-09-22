830bcabdd673f367d902d08016d2baaa1a7b984b
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-09-23 05:22:12 +0700

    Add CRUD for model Clients

---
+++ mailing/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from mailing.apps import MailingConfig
+from mailing.views import (
+    ClientCreateView,
+    ClientDeleteView,
+    ClientDetailView,
+    ClientListView,
+    ClientUpdateView,
+)
+
+app_name = MailingConfig.name
+
+urlpatterns = [
+    path("clients/", ClientListView.as_view(), name="client_list"),
+    path("clients/<int:pk>/", ClientDetailView.as_view(), name="client_detail"),
+    path("clients/create/", ClientCreateView.as_view(), name="client_create"),
+    path("clients/<int:pk>/update/", ClientUpdateView.as_view(), name="client_update"),
+    path("clients/<int:pk>/delete/", ClientDeleteView.as_view(), name="client_delete"),
+]
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
-# Create your views here.
+from mailing.forms import ClientForm
+from mailing.models import Client
+
+
+class ClientListView(LoginRequiredMixin, ListView):
+    model = Client
+    template_name = "mailing/client_list.html"
+
+    def get_queryset(self):
+        # Пользователь видит только своих клиентов
+        return Client.objects.filter(owner=self.request.user)
+
+
+class ClientDetailView(LoginRequiredMixin, DetailView):
+    model = Client
+    template_name = "mailing/client_detail.html"
+
+    def get_queryset(self):
+        return Client.objects.filter(owner=self.request.user)
+
+
+class ClientCreateView(LoginRequiredMixin, CreateView):
+    model = Client
+    form_class = ClientForm
+    template_name = "mailing/client_form.html"
+    success_url = reverse_lazy("mailing:client_list")
+
+    def form_valid(self, form):
+        client = form.save(commit=False)
+        client.owner = self.request.user  # Привязываем клиента к текущему владельцу
+        client.save()
+        return super().form_valid(form)
+
+
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
+    model = Client
+    form_class = ClientForm
+    template_name = "mailing/client_form.html"
+    success_url = reverse_lazy("mailing:client_list")
+
+    def get_queryset(self):
+        return Client.objects.filter(owner=self.request.user)
+
+
+class ClientDeleteView(LoginRequiredMixin, DeleteView):
+    model = Client
+    template_name = "mailing/client_confirm_delete.html"
+    success_url = reverse_lazy("mailing:client_list")
+
+    def get_queryset(self):
+        return Client.objects.filter(owner=self.request.user)
